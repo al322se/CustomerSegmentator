e0783c331d3bd7d17db61bc5c230756524a1bc1e
semaleksandrov <semaleksandrov@ozon.ru>
2024-02-02 15:01:40 +0300

    remove not need

--- CustomerSegmentator/CustomerSegmentator/Controllers/CustomerArrivedEventController.cs
+++ CustomerSegmentator/CustomerSegmentator/Controllers/CustomerArrivedEventController.cs
@@ -15,30 +15,11 @@ namespace CustomerSegmentator.Controllers
             context.Database.Migrate();
         }
 
-        // GET: CustomerArrivedEvent
         public async Task<IActionResult> Index()
         {
               return _context.CustomerArrivedEvent != null ? 
                           View(await _context.CustomerArrivedEvent.ToListAsync()) :
                           Problem("Entity set 'CustomerSegmentatorContext.CustomerArrivedEvent'  is null.");
-        }
-
-        // GET: CustomerArrivedEvent/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.CustomerArrivedEvent == null)
-            {
-                return NotFound();
-            }
-
-            var customerArrivedEvent = await _context.CustomerArrivedEvent
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (customerArrivedEvent == null)
-            {
-                return NotFound();
-            }
-
-            return View(customerArrivedEvent);
         }
 
         // GET: CustomerArrivedEvent/Create
@@ -47,9 +28,6 @@ namespace CustomerSegmentator.Controllers
             return View();
         }
 
-        // POST: CustomerArrivedEvent/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Quantity,Segment,Tarriff,IsCash")] CustomerArrivedEvent customerArrivedEvent)
@@ -62,99 +40,6 @@ namespace CustomerSegmentator.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(customerArrivedEvent);
-        }
-
-        // GET: CustomerArrivedEvent/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.CustomerArrivedEvent == null)
-            {
-                return NotFound();
-            }
-
-            var customerArrivedEvent = await _context.CustomerArrivedEvent.FindAsync(id);
-            if (customerArrivedEvent == null)
-            {
-                return NotFound();
-            }
-            return View(customerArrivedEvent);
-        }
-
-        // POST: CustomerArrivedEvent/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Quantity,Segment,Tarriff,IsCash")] CustomerArrivedEvent customerArrivedEvent)
-        {
-            if (id != customerArrivedEvent.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(customerArrivedEvent);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CustomerArrivedEventExists(customerArrivedEvent.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(customerArrivedEvent);
-        }
-
-        // GET: CustomerArrivedEvent/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.CustomerArrivedEvent == null)
-            {
-                return NotFound();
-            }
-
-            var customerArrivedEvent = await _context.CustomerArrivedEvent
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (customerArrivedEvent == null)
-            {
-                return NotFound();
-            }
-
-            return View(customerArrivedEvent);
-        }
-
-        // POST: CustomerArrivedEvent/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.CustomerArrivedEvent == null)
-            {
-                return Problem("Entity set 'CustomerSegmentatorContext.CustomerArrivedEvent'  is null.");
-            }
-            var customerArrivedEvent = await _context.CustomerArrivedEvent.FindAsync(id);
-            if (customerArrivedEvent != null)
-            {
-                _context.CustomerArrivedEvent.Remove(customerArrivedEvent);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CustomerArrivedEventExists(int id)
-        {
-          return (_context.CustomerArrivedEvent?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
--- CustomerSegmentator/CustomerSegmentator/Program.cs
+++ CustomerSegmentator/CustomerSegmentator/Program.cs
@@ -27,7 +27,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=CustomerArrivedEvent}/{action=Create}/{id?}");
 
 
 app.Run();
